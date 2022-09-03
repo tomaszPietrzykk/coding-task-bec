c494c1b0fc7a8fe2d2e65431cec4bf1a7ea42eda
tomaszpietrzykk <pietrzyktomek@gmail.com>
2022-09-03 19:17:34 +0200

    fixed some pom.xml problem

--- src/test/java/dk/bec/codingtask/CodingTaskApplicationTests.java
+++ src/test/java/dk/bec/codingtask/CodingTaskApplicationTests.java
@@ -1,6 +1,6 @@
 package dk.bec.codingtask;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
